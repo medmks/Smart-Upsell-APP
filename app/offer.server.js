f342711fef085583d8e635820aefbc4a74866080
Elabiade <abdomel2002@email.com>
2024-02-08 15:25:53 +0100

    v1-post-pur

---
+++ app/offer.server.js
@@ -0,0 +1,43 @@
+const OFFERS = [
+    {
+      id: 1,
+      title: "One time offer",
+      productTitle: "The S-Series Snowboard",
+      productImageURL:
+        "https://cdn.shopify.com/s/files/1/0", // Replace this with the product image's URL.
+      productDescription: ["This PREMIUM snowboard is so SUPER DUPER awesome!"],
+      originalPrice: "699.95",
+      discountedPrice: "699.95",
+      changes: [
+        {
+          type: "add_variant",
+          variantID: 123456789, // Replace with the variant ID.
+          quantity: 1,
+          discount: {
+            value: 15,
+            valueType: "percentage",
+            title: "15% off",
+          },
+        },
+      ],
+    },
+  ];
+  
+  /*
+   * For testing purposes, product information is hardcoded.
+   * In a production application, replace this function with logic to determine
+   * what product to offer to the customer.
+   */
+  export function getOffers() {
+    return OFFERS;
+  }
+  
+  /*
+   * Retrieve discount information for the specific order on the backend instead of relying
+   * on the discount information that is sent from the frontend.
+   * This is to ensure that the discount information is not tampered with.
+   */
+  export function getSelectedOffer(offerId) {
+    return OFFERS.find((offer) => offer.id === offerId);
+  }
+  
